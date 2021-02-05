c113e6d1bad0cab87f2d0fb4049f01141c183202
Abdulla Al Maruf <abdullaalmaruf763797@gmail.com>
2021-02-06 00:03:46 +0600

    complete

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+//let bird;
+// function add (num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// function add (num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+// const address = {name: 'Rajabazar', houseNo: '59/A/1'};
+// console.log(address.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+// let ages = [2, 5, 9];
+// console.log(ages[11]);
+ 
---
+++ scope.js
@@ -0,0 +1,16 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    //console.log(bonus);
+    if (result > 9) {
+         mood = 'happy';
+        mood = 'fanky';
+        mood = 'fishy';
+    }
+    console.log(mood);
+    return result;
+    
+}
+const output = sum(3, 7);
+// console.log(output);
